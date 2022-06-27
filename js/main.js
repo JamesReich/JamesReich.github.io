62a600956d3a876e85fa26997b6015d32e7d4fab
James Reich <james@jreich.dev>
2022-06-26 17:01:39 -0700

    Video background

--- js/main.js
+++ js/main.js
@@ -137,3 +137,46 @@ failBtn.addEventListener('click', () => {
 
 })
 
+    ///////////////////////////
+    //       VIDEO BG
+    ///////////////////////////
+
+    (function () {
+
+        var bv = new Bideo();
+        bv.init({
+            // Video element
+            videoEl: document.querySelector('#background_video'),
+
+            // Container element
+            container: document.querySelector('video-container'),
+
+            // Resize
+            resize: true,
+
+            autoplay: true,
+
+            isMobile: window.matchMedia('(max-width: 768px)').matches,
+
+            playButton: document.querySelector('#play'),
+            pauseButton: document.querySelector('#pause'),
+
+            // Array of objects containing the src and type
+            // of different video formats to add
+            src: [
+                {
+                    src: 'video/particles.mp4',
+                    type: 'video/mp4'
+                },
+                {
+                    src: 'night.webm',
+                    type: 'video/webm;codecs="vp8, vorbis"'
+                }
+            ],
+
+            // What to do once video loads (initial frame)
+            onLoad: function () {
+                document.querySelector('#video_cover').style.display = 'none';
+            }
+        });
+    }());
