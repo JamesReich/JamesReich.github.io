85db9d19f003d75e548c18801e22e91841edbbe4
James Reich <james@jreich.dev>
2022-07-03 20:01:49 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -105,13 +105,6 @@ document.getElementById('contact-form').addEventListener('submit', (ev) => {
 
 });
 
-
-
-
-
-
-
-
 window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
